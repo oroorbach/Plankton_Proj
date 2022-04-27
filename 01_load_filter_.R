1988bc3211dc969cbe4fe3aac63d5f0c7d926e4f
Roorbach <Olivia.Roorbach@FloridaDEP.gov>
2022-04-27 08:41:04 -0400

    updates

--- 01_load_filter_.R
+++ 01_load_filter_.R
@@ -1,4 +1,4 @@
-
+install.packages()
 ## setwd('C:/Users/roorbach_o/Documents/R/win-library/4.0')
 #### if you created the project right, here function should be able to find you #####
 
@@ -21,6 +21,7 @@ library(kableExtra)
 library(khroma)
 library(mosaic)
 library(psych)
+library(writexl)
 
 ## ???
 here::here('data')
@@ -41,18 +42,39 @@ dat2 <- dat %>%
          sample_date, 
          component_short, 
          result) %>%
-  filter(station_code %in% c("GTMLMNUT", "GTMGRNUT"))
-
+  filter(station_code %in% c("GTMLMNUT", "GTMGRNUT"),
+        !(component_short %in% c("WIND_D", "SECCHI")))
 # selecting out just the year fomr the time_date column so I can then add it to the staion-code column
 
 dat3 <- dat2 %>%
-  mutate(year = year(sample_date))
+  mutate(year = year(sample_date),
+         month = month(sample_date))
 
 # adding year to the station_code 
 
 dat3$uniq <- paste(dat3$station_code, dat3$year, sep = "_")
+dat3$uniqq <- paste(dat3$uniq, dat3$month, sep = '_')
 
 
+#RE-order and pivot to format for gabby's needs
+dat3$result <- as.numeric(dat3$result)
+
+-----could also use mutate(result = as.numeric(result)) within function???
+
+## selecting out the column I need  
+dat_wider <- dat3 %>%
+select(uniqq,
+       component_short,
+       result)
+
+# widening data for gabby's formatting 
+plank_dat <- dat_wider %>%
+  pivot_wider(names_from = "component_short",
+               values_from = "result")
+
+
+# save as excel for Gabby to use in primer
+write_xlsx(plank_dat, "LM_GL4_Plank.xlsx")
 
 
 
